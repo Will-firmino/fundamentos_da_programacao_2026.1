1ef6615d5573a7911aa5a1b35442bd3dab56ce52
william firmino <prof.williamfirmino@gmail.com>
2026-03-27 09:47:44 -0300

    Alterações do dia 27/03/2026

--- aula_10/estrutura_pizzaria_promocoes.py
+++ aula_10/estrutura_pizzaria_promocoes.py
@@ -9,7 +9,18 @@ print(f" O sabor escolhido da pizza é {pizza_sabor}, o tamanho é {pizza_tamanh
 
 # Comprando qualquer pizza e qualquer tamanho no sábado,
 # o refri é gratuito.
-
+if dia_semana == "sabado":
+    print(f"🍕Pedido aceito com sucesso!")
+    print(f"O Refri hoje é por conta da casa!.")
+elif dia_semana == "domingo":
+    print(f"🍕Pedido aceito com sucesso!")
+    print(f"O Frete e o Refri hoje é por conta da casa!.")
+elif pizza_sabor == "calabresa" and pizza_tamanho == "média":
+    print(f"🍕Pedido aceito com sucesso!")
+    print(f"O Frete hoje é por conta da casa!.")
+    
+    
+    
 # Comprando uma pizza de calabresa tamanho médio, em qualquer dia, o frete é gratuito.
 
 # Comprando qualquer pizza de qualquer tamanho no domingo, o frete e o refri são gratuitos.
